657c1c5f4f732babd1c092de8fdaffe0ed94e830
park-byeong-gwan <impbk2002@gmail.com>
2024-05-19 10:49:44 +0900

    remove visionOS target since swift5.7 does not have visionOS

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
         .tvOS(.v13),
         .macOS(.v10_15),
         .watchOS(.v6),
-        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
---
+++ Sources/Tetra/Concurrency/CompatDiscardigTaskGroup.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 박병관 on 5/16/24.
+//
+
+import Foundation
