e6db470a51ed84c309f2bc4d275113630b6dd2d8
park-byeong-gwan <impbk2002@gmail.com>
2024-05-30 14:46:45 +0900

    use StrictConcurrency

--- Package.swift
+++ Package.swift
@@ -29,7 +29,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Tetra",
-            dependencies: []
+            dependencies: [],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency=complete"),
+            ]
         ),
         .testTarget(
             name: "TetraTests",
